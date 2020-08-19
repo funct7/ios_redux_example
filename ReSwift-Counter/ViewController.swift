779b4e8c7addd4b782bc87a45f3151b3b1eee715
Joshua Park <wmpark@knowre.com>
2020-08-19 11:13:21 +0900

    [reswift-counter] Add: Basic UI

--- ReSwift-Counter/ViewController.swift
+++ ReSwift-Counter/ViewController.swift
@@ -7,14 +7,53 @@
 //
 
 import UIKit
+import ReSwift
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, StoreSubscriber {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    override
+    func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        store.subscribe(self) {
+            $0.select { Counter.selector(state: $0) }
+        }
+    }
+    
+    override
+    func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        store.unsubscribe(self)
     }
 
-
+    func newState(state: Counter.State) {
+        countLabel.text = "\(state.value)"
+    }
+    
+    // MARK: Private
+    
+    private var state: Counter.State { Counter.selector(state: store.state) }
+    
+    @IBOutlet
+    private weak var countLabel: UILabel!
+    
+    @IBAction
+    private func decrementAction(_ sender: Any) {
+        store.dispatchAction(.decrement)
+    }
+    
+    @IBAction
+    private func incrementAction(_ sender: Any) {
+        store.dispatchAction(.increment)
+    }
+    
 }
 
+private extension Store where State == AppState {
+    
+    func dispatchAction(_ action: Counter.Action) {
+        dispatch(action)
+    }
+    
+}
